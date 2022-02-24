4663dd57384d53ca2f3219d58c435116fef03df6
YAOYINGYING <yingying.yao@witsion.com>
2022-02-24 19:16:28 +0800

    remove deploy

--- deploy/scripts/update-resource-mirror.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -aeuo pipefail
-
-# sed -i'.bak'  <<< for it to work on both GNU & BSD
-sed -i'.bak' '/dl-cdn.alpinelinux.org/s/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' .env
